dab0b679ddfe060081e50ceed18f82540ec6a375
Vickytg1 <162087823+Vickytg1@users.noreply.github.com>
2024-08-14 21:53:27 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,8 +7,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
+    bucket         = "jelbucketforeks"
+    region         = "ap-southeast-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
